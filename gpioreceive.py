fe76582d079478ac4edbbb65eadadcd64309fe5d
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-08-08 03:22:18 +0000

    updated to add picamera and code to listen to GPIO pin 3 to go high

--- gpioreceive.py
+++ gpioreceive.py
@@ -1,6 +1,17 @@
+import picamera
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.IN)
+
+while True:
+	if GPIO.input(3):
+		with picamera.PiCamera() as camera:
+			camera.resolution = (2592,1944)
+			camera.start_preview()
+			time.sleep(2)
+			camera.capture('test.jpg')
+	else:
+		time.sleep(0.1)
 
 GPIO.cleanup()
