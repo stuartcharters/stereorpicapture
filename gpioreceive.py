02a01038983c6902806dd3424ab6624d3d8990b6
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-08-09 13:34:51 +1200

    added debug

--- gpioreceive.py
+++ gpioreceive.py
@@ -3,14 +3,18 @@ import picamera
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
+	print "Before wait"
+	print GPIO.input(3)
 	GPIO.wait_for_edge(3,GPIO.RISING)
 	with picamera.PiCamera() as camera:
 		camera.resolution = (2592,1944)
-		camera.start_preview()
+		#camera.start_preview()
+		print "after wait"
+		print GPIO.input(3)
 		time.sleep(2)
-		camera.capture('test.jpg')
+		#camera.capture('test.jpg')
 
 GPIO.cleanup()
