7d56fb84551d5a4cfff8bcb89b31ca11208d984f
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-08-09 13:52:40 +1200

    changed delay to one second

--- gpioreceive.py
+++ gpioreceive.py
@@ -9,12 +9,14 @@ while True:
 	print "Before wait"
 	print GPIO.input(3)
 	GPIO.wait_for_edge(3,GPIO.RISING)
+	print "after wait"
+	print GPIO.input(3)
 	with picamera.PiCamera() as camera:
 		camera.resolution = (2592,1944)
 		#camera.start_preview()
-		print "after wait"
-		print GPIO.input(3)
-		time.sleep(2)
+		#print "after wait"
+		#print GPIO.input(3)
+		time.sleep(1)
 		#camera.capture('test.jpg')
 
 GPIO.cleanup()
