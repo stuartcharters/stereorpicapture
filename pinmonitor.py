a3cd9db6475ab3b61bbc805d786ed1b9d20557b6
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-08-11 11:56:39 +1200

    updated to continually read from the pin

--- pinmonitor.py
+++ pinmonitor.py
@@ -2,5 +2,7 @@ import RPi.GPIO as GPIO
 #GPIO.cleanup()
 pin = 11
 GPIO.setmode(GPIO.BOARD)
-print GPIO.input(pin)
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+while True:
+    print GPIO.input(pin)
 GPIO.cleanup()
