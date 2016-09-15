0f52e82accc97af3b240a34534814ba3436d88e1
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-09-16 10:17:22 +1200

    updated to read ivey images and to save output rather than display

--- depthmap.py
+++ depthmap.py
@@ -1,11 +1,12 @@
 import numpy as np
-    2 import cv2
-    3 from matplotlib import pyplot as plt
-    4
-    5 imgL = cv2.imread('tsukuba_l.png',0)
-    6 imgR = cv2.imread('tsukuba_r.png',0)
-    7
-    8 stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-    9 disparity = stereo.compute(imgL,imgR)
-   10 plt.imshow(disparity,'gray')
-   11 plt.show()
+import cv2
+from matplotlib import pyplot as plt
+
+imgL = cv2.imread('left-ir-2.jpg',0)
+imgR = cv2.imread('right-ir-2.jpg',0)
+
+stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+disparity = stereo.compute(imgL,imgR)
+cv2.imwrite('depthmap.jpg',disparity)
+#plt.imshow(disparity,'gray')
+#plt.show()
