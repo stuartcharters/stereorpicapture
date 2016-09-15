7521957607b36a37cd4aaa7928f3a771a8ccd697
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-09-16 10:36:05 +1200

    number disparities 32 windows size 7

--- depthmap.py
+++ depthmap.py
@@ -6,7 +6,7 @@ imgL = cv2.imread('left-ir-2.jpg',0)
 imgR = cv2.imread('right-ir-2.jpg',0)
 
 #stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,ndisparities=8, SADWindowSize=7)
+stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,ndisparities=32, SADWindowSize=7)
 disparity = stereo.compute(imgL,imgR)
 cv2.imwrite('depthmap.jpg',disparity)
 #plt.imshow(disparity,'gray')
