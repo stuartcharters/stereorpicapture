6b437867a75b8953946dfe0d802d42a0f944d60f
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-09-16 14:48:25 +1200

    filename change

--- depthmap.py
+++ depthmap.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-imgL = cv2.imread('left-ir-2.jpg',0)
-imgR = cv2.imread('right-ir-2.jpg',0)
+imgL = cv2.imread('left.jpg',0)
+imgR = cv2.imread('right.jpg',0)
 
 #stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
 stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,ndisparities=16, SADWindowSize=5)
