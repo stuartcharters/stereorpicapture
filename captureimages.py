7ee2098df4aedaec6148ffd2c1a7a066cef4c433
stuartcharters <stuart.charters@lincoln.ac.nz>
2016-11-03 18:00:03 +1000

    put a -1 on the photostotake on the slave wait counter to resolve an issue with it not exiting after taking all the photos

--- captureimages.py
+++ captureimages.py
@@ -44,7 +44,7 @@ def main(argv):
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(pin, GPIO.RISING)
         GPIO.add_event_callback(pin,takepic)
-        while (piccount < photostotake):
+        while (piccount < (photostotake-1)):
             time.sleep(0.01)
     GPIO.cleanup()
 
